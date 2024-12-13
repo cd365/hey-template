6050ec95828f1b6a2a703f95ec8f29accc68102d
cosasce <cosasce@gmail.com>
2024-12-13 14:51:51 +0800

    program version

--- values/program.go
+++ values/program.go
@@ -1,10 +1,10 @@
 package values
 
 const (
-	Version = "v0.6.0" // 程序版本号
+	Version = "v0.6.3" // 程序版本号
 )
 
 var (
-	BuildAt  string // 程序构建时间 20060102150405
-	CommitId string // 程序版本控制id 02fe8c0c3a20f1ee9b658bfb8530ed5ca6670285
+	BuildAt  string // 编译参数-程序构建时间 20060102150405
+	CommitId string // 编译参数-程序版本控制id 02fe8c0c3a20f1ee9b658bfb8530ed5ca6670285
 )
